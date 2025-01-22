901b30e7acb6a2f34e216125da8d78a85136cf8d
Nick Sherrill <nsherrill@alphatheory.com>
2025-01-22 15:33:50 -0500

    add a method to generate lines

--- src/main/kotlin/com/alphatheory/demoprojreleasesnsherrill/controllers/ControllerA.kt
+++ src/main/kotlin/com/alphatheory/demoprojreleasesnsherrill/controllers/ControllerA.kt
@@ -2,12 +2,27 @@ package com.alphatheory.demoprojreleasesnsherrill.controllers
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.math.absoluteValue
+import kotlin.random.Random
 
 @RestController
 class ControllerA {
 
     @RequestMapping("api/v1/letter")
-    fun giveMeLetter(): String{
+    fun giveMeLetter(): String {
         return "your letter is ${('a'..'z').random()}"
+    }
+
+    @RequestMapping("api/v1/gen-file")
+    fun genFile(): String {
+        val numLines = Random.nextInt(10, 1000)
+        val now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME).filter { it.isLetterOrDigit() }
+        File("generatedLines_$now.txt").writeText(
+            List(numLines) { Random.nextLong().absoluteValue.toString() }.joinToString("\n")
+        )
+        return "wrote $numLines lines to file"
     }
 }
