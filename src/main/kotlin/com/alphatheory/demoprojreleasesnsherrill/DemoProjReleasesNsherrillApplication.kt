dff8e96e21da74224f3b81c6305d99b995fdc5e7
Nick Sherrill <nsherrill@alphatheory.com>
2025-01-22 14:49:53 -0500

    init commit

---
+++ src/main/kotlin/com/alphatheory/demoprojreleasesnsherrill/DemoProjReleasesNsherrillApplication.kt
@@ -0,0 +1,11 @@
+package com.alphatheory.demoprojreleasesnsherrill
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoProjReleasesNsherrillApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoProjReleasesNsherrillApplication>(*args)
+}
---
+++ src/test/kotlin/com/alphatheory/demoprojreleasesnsherrill/DemoProjReleasesNsherrillApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphatheory.demoprojreleasesnsherrill
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoProjReleasesNsherrillApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
